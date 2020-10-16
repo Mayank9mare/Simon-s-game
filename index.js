b9037562f336139d07e8557ab853590dde2b5689
Mayank Raj <mayank42293@gmail.com>
2020-10-16 21:27:53 +0530

    basic design

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var r=$("#red");
-var y=$("#yellow");
-var b=$("#blue");
-var g=$("#green");
+
+var level;
+var colors=["red","blue","green","yellow"];
+var seq=[];
+var enter=[];
+var cur_level =0;
 
 function wrong(){
     $("h1").text("Game Over , press A to start new game");
@@ -9,54 +11,76 @@ function wrong(){
     setTimeout(function(){$("body").removeClass("gameover")},100);
     var audio =new Audio("sounds/wrong.mp3");
     audio.play();
+    seq=[];
+    cur_level=0;
+    level=0;
 
 }
 
 $(".buttons").click(function(){
     var cb=this.id;
-    $("#"+cb).addClass("pressed");
-    setTimeout(function(){$("#"+cb).removeClass("pressed")},100);
-    switch(cb){
-        case "green":
-            var aud= new Audio("sounds/green.mp3");
-            aud.play();
-        case "blue":
-            var aud= new Audio("sounds/blue.mp3");
-            aud.play();
-        case "yellow":
-            var aud= new Audio("sounds/yellow.mp3");
-            aud.play();
-        case "red":
-            var aud= new Audio("sounds/red.mp3");
-            aud.play();
-            
+    
+    animate(cb);
+    sound(cb);
+    if(seq.length===0){
+        wrong();
     }
+    else{
+    if(seq[cur_level]===cb){
+        if(seq.length===(cur_level+1)){
+            setTimeout(nextSequence(),1000);
+        }
+        else{
+            cur_level++;
+        }
+    }
+    else{
+        wrong();
+    }
+}
     
     
     
 });
 
-    
-
-function start(a){
-    var R=Math.random();
-    var rn=Math.floor(R*4)+1;
-    var col;
-    switch(rn){
-        case 1:
-            col="red";
-        case 2:
-            col="blue";
-        case 3:
-            col="yellow";
-        case 4:
-            col="green";
-    
-
-
-    }
-    return col;
+function sound(a){
+    var aud=new Audio("sounds/"+a+".mp3");
+    aud.play();
 }
+function animate(a){
+    $("#"+a).addClass("pressed");
+    setTimeout(function(){$("#"+a).removeClass("pressed")},100);
+
+}
+function start(){
+    var lol=4*Math.random() ;
+    var t=Math.floor(lol);
+    seq.push(colors[t]);
+    $("#"+colors[t]).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("h1").text("Level "+level.toString());
+    
+
+
+
+}
+function nextSequence(){
+    level++;
+    cur_level=0;
+    $("h1").text("Level"+level.toString());
+    var lol=4*Math.random();
+    var t=Math.floor(lol);
+    console.log(colors[t]);
+    seq.push(colors[t]);
+    $("#"+colors[t]).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("h1").text("Level "+level.toString());
+
+}
+
+
+
+    
+
+
 
     
     
@@ -69,7 +93,9 @@ function start(a){
 document.addEventListener("keydown",function(event){
     
    if(event.key==="a"){
+       level=1;
        start();
+       
    }
    else{
        
